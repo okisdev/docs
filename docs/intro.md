759ba2a7badcd8be5e1b9e6e5b3d148800c6602a
Harry Yep <github@hode.uk>
2022-03-31 16:37:22 +0100

    Docusaurus 2

---
+++ docs/intro.md
@@ -0,0 +1,7 @@
+---
+sidebar_position: 1
+---
+
+# Hello World
+
+Hello World
---
+++ docusaurus.config.js
@@ -0,0 +1,166 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+const math = require('remark-math');
+const katex = require('rehype-katex');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+    title: "Harry Yep's Documentations",
+    tagline: "Harry Yep's Documentations",
+    url: 'https://docs.harrly.com',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'https://cdn.harrly.com/favicon.ico',
+    organizationName: 'Harry-Yep',
+    projectName: 'Website_Harrly_Docs',
+
+    stylesheets: [
+        {
+            href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+            type: 'text/css',
+            integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+            crossorigin: 'anonymous',
+        },
+    ],
+
+    scripts: [
+        {
+            async: true,
+            src: 'https://splitbee.hode.co.uk/sb.js',
+            'data-token': 'XLF8QST151A6',
+        },
+        {
+            async: true,
+            src: 'https://www.googletagmanager.com/gtag/js?id=UA-183901953-4',
+        },
+    ],
+
+    plugins: [
+        [
+            require.resolve('@easyops-cn/docusaurus-search-local'),
+            {
+                hashed: true,
+            },
+        ],
+    ],
+
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    editUrl: 'https://github.com/Harry-Yep/Website_Harrly_Docs/edit/main/docs/',
+                    remarkPlugins: [math],
+                    rehypePlugins: [katex],
+                },
+                blog: {
+                    showReadingTime: true,
+                    editUrl: 'https://github.com/Harry-Yep/Website_Harrly_Docs/edit/main/blog/',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
+    ],
+
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            // announcementBar: {
+            //     id: '',
+            //     content: ``,
+            // },
+            metadata: [
+                { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
+                { name: 'description', content: "Harry Yep's Documentations" },
+                { name: 'author', content: 'Harry Yep' },
+                { name: 'copyright', content: `Copyright © ${new Date().getFullYear()} Harry Yep.` },
+                { property: 'og:title', content: "Harry Yep's Documentations" },
+                { property: 'og:description', content: "Harry Yep's Documentations" },
+            ],
+            navbar: {
+                title: "Harry Yep's Documentations",
+                logo: {
+                    alt: 'logo',
+                    src: 'https://cdn.harrly.com/favicon.ico',
+                },
+                items: [
+                    {
+                        type: 'dropdown',
+                        position: 'left',
+                        label: 'Docs',
+                        items: [
+                            {
+                                label: 'Notion Blog React',
+                                to: '/docs/intro',
+                            },
+                            {
+                                label: 'Notion Photo React',
+                                to: '/docs/intro',
+                            },
+                        ],
+                    },
+                    {
+                        to: '/blog',
+                        label: 'Blog',
+                        position: 'left',
+                    },
+                    {
+                        href: 'https://github.com/Harry-Yep',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                ],
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'Docs',
+                        items: [
+                            {
+                                label: 'Notion Blog React',
+                                to: '/docs/intro',
+                            },
+                            {
+                                label: 'Notion Photo React',
+                                to: '/docs/intro',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'About',
+                        items: [
+                            {
+                                label: 'Blog',
+                                href: 'https://blog.harrly.com/',
+                            },
+                            {
+                                label: 'GitHub',
+                                href: 'https://github.com/Harry-Yep',
+                            },
+                            {
+                                label: 'Privacy Policy ',
+                                href: 'https://www.harrly.com/privacy-policy/',
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} Harry Yep. All rights reserved.`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        }),
+};
+
+module.exports = config;
