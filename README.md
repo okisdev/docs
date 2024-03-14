095f0e601977ea0c8ff26ed50a125269c801d3f0
Harry Yep <git@hode.co.uk>
2024-03-14 21:50:49 +0800

    feat: use nextra

---
+++ README.md
@@ -0,0 +1,7 @@
+# okisdev docs
+
+This repository contains the documentation for the okisdev project.
+
+## LICENSE
+
+[MIT License](./LICENSE)
--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.jsx',
+    latex: true,
 });
 
 module.exports = withNextra();
