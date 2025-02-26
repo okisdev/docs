b4f9ab771a6470950344d82019aea7859823c29d
Harry Yep <git@hode.co.uk>
2025-02-26 17:24:06 +0800

    fix: redirect

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,36 @@ const config = {
       destination: '/docs/apis',
       permanent: true,
     },
+    {
+      source: '/apis',
+      destination: '/docs/apis',
+      permanent: true,
+    },
+    {
+      source: '/chat',
+      destination: '/docs/chat',
+      permanent: true,
+    },
+    {
+      source: '/notion-blog-react',
+      destination: '/docs/notion-blog-react',
+      permanent: true,
+    },
+    {
+      source: '/notion-photo-react',
+      destination: '/docs/notion-photo-react',
+      permanent: true,
+    },
+    {
+      source: '/rail',
+      destination: '/docs/rail',
+      permanent: true,
+    },
+    {
+      source: '/tfl-tube',
+      destination: '/docs/tfl-tube',
+      permanent: true,
+    },
   ],
   images: {
     remotePatterns: [
